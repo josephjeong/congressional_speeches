21d722dee69cb9fc635d39cdb8e88c85b2311d4e
Joseph Jeong <joe.b.jeong@gmail.com>
2022-06-05 17:49:16 +0800

    bioname included

--- speakermap.py
+++ speakermap.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 speakers = pd.read_csv("data/HSall_members.csv")
 speakers = speakers.dropna(subset=["died", "born"])
@@ -17,7 +16,7 @@ speakers = pd.merge(speakers, parties, on="party_code")
 speakers[["lastname", "firstname"]] = speakers["bioname"].str.split(",", n=1, expand=True)
 speakers.loc[speakers["chamber"] == "House", "house"] = "HOUSE OF REPRESENTATIVES"
 speakers.loc[speakers["chamber"] == "Senate", "house"] = "SENATE"
-speakers = speakers[["lastname", "state_abbrev", "house", "party_name"]]
+speakers = speakers[["lastname", "bioname", "state_abbrev", "house", "party_name"]]
 # s = df["bioname"].str.split(",", n=1, expand=True)
 # print(s)
 
@@ -26,58 +25,15 @@ speakers = speakers[["lastname", "state_abbrev", "house", "party_name"]]
 df = pd.read_csv("data/speeches.csv", delimiter="|")
 df["lastname"] = df["clean_names"].str.extract(r"\b(\w*?)$")
 # df.drop(columns=["clean_names", "speeches"], inplace=True)
-# print(df)
+df.drop(columns=["clean_names"], inplace=True)
 
 df['lastname'] = df['lastname'].fillna(0)
 df['house'] = df['house'].fillna(0)
 
-# df.to_csv("")
+print(df.shape, speakers.shape)
+df.drop_duplicates(inplace=True)
+speakers.drop_duplicates(inplace=True)
+print(df.shape, speakers.shape)
 
-# n = 50000
-# list_df = [df[i:i+n] for i in range(0,df.shape[0],n)]
-# print(len(list_df))
-
-# result_dfs = []
-# for i, small_df in enumerate(list_df):
-#     result_dfs.append(pd.merge(small_df, speakers, on=["lastname", "house"], how="left"))
-#     print("another one", i)
-# df = pd.concat(result_dfs)
-
-
-# # df= pd.merge(df, speakers, on=["lastname", "house"], how="left")
-# df.to_csv("speakermap.csv", sep="|", index=False, header=True)
-# print(df)
-
-speakers.to_csv("yourdata.csv", sep="|")
-df.to_csv("yourdata2.csv", sep="|")
-
-del(speakers)
-del(df)
-
-df1 = pd.read_csv("yourdata.csv", delimiter="|")
-df2 = pd.read_csv("yourdata2.csv", delimiter="|")
-
-# creating a empty bucket to save result
-df_result = pd.DataFrame(columns=(df1.columns.append(df2.columns)).unique())
-df_result.to_csv("df3.csv",index_label=False, sep="|")
-
-# save data which only appear in df1 # sorry I was doing left join here. no need to run below two line.
-# df_result = df1[df1.Colname1.isin(df2.Colname2)!=True]
-# df_result.to_csv("df3.csv",index_label=False, mode="a")
-
-# deleting df2 to save memory
-del(df2)
-
-def preprocess(x):
-    print("preprocessing!")
-    df2=pd.merge(df1,x, on=["lastname", "house"], how="left")
-    df2.to_csv("df3.csv", mode="a", header=False, index=False, sep="|")
-
-reader = pd.read_csv("yourdata2.csv", chunksize=10000, delimiter="|") # chunksize depends with you colsize
-
-[preprocess(r) for r in reader]
-
-"""
-1196714
-1,412,423,881
-"""
+df= pd.merge(df, speakers, on=["lastname", "house"], how="left")
+df.to_csv("speakermap.csv", sep="|", index=False, header=True)
