4996c4626111776d169b862a5521eeb200d7052e
Joseph Jeong <joseph.jeong@student.unsw.edu.au>
2022-06-21 23:04:22 +1000

    additional instructions for congressional dates

--- src/check_init.py
+++ src/check_init.py
@@ -13,8 +13,8 @@ def check_init() -> None:
             print(f"Creating path: {folder}")
             os.makedirs(folder)
     files_to_exist = {
-        "data/hein_dataset/hein-bound/043_SpeakerMap.txt": "https://primoa.library.unsw.edu.au/primo-explore/fulldisplay?docid=UNSW_ALMA61158502230001731&vid=UNSWS&lang=en_US&context=L\nAnd please remove all rows where the date format is not: %b %d, %Y",
-        "data/HSall_dataset/congress_dates.csv": "https://github.com/shmcminn/congress-begin-end-dates/blob/master/congress-begin-end-dates.csv",
+        "data/hein_dataset/hein-bound/043_SpeakerMap.txt": "https://primoa.library.unsw.edu.au/primo-explore/fulldisplay?docid=UNSW_ALMA61158502230001731&vid=UNSWS&lang=en_US&context=L",
+        "data/HSall_dataset/congress_dates.csv": "https://github.com/shmcminn/congress-begin-end-dates/blob/master/congress-begin-end-dates.csv\nAnd please remove all rows where the date format is not: %b %d, %Y",
         "data/HSall_dataset/HSall_members.csv": "https://voteview.com/articles/data_help_members",
         "data/HSall_dataset/HSall_parties.csv": "https://voteview.com/articles/data_help_parties"
     }
